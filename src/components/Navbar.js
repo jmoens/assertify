dfdce6e0088949f1c15c30519d046bb43f2b4593
Lisa <lisaobrien2009@hotmail.com>
2020-03-07 19:18:53 -0800

    textbox

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link, NavLink } from 'react-router-dom';
+import './Styles.css'
 
 class Navbar extends Component {
     state ={
@@ -8,12 +9,12 @@ class Navbar extends Component {
     render() {
         return (
             <div>
-                <nav className="nav-wrapper deep-purple lighten-1">
+                <nav className="nav-wrapper deep-purple lighten-1 list">
                     <div className="container">
                         <ul className="right">
-                            <li><Link to="/">Home</Link></li>
-                            <li><NavLink to="/translator">TextBox</NavLink></li>
-                            <li><NavLink to="/resources">Resources</NavLink></li>
+                            <li ><Link to="/" className="list">Home</Link></li>
+                            <li className="list"><NavLink to="/translator" className="list">TextBox</NavLink></li>
+                            <li><NavLink to="/resources" className="list">Resources</NavLink></li>
                         </ul>
                     </div>
                 </nav>
