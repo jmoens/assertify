ade873268e1ae655ba18d9ec6f0f1424b1f24f1a
Lisa <lisaobrien2009@hotmail.com>
2020-03-08 10:20:21 -0700

    styling and async

--- src/components/TextPage.js
+++ src/components/TextPage.js
@@ -22,14 +22,14 @@ class TextPage extends Component {
 
     onSubmit = (e) => {
         e.preventDefault();
+        let text = this.state.entry
         this.setState({
-            previousEntry: this.state.entry
-        })
-        this.beginAnalysis();
+            previousEntry: text,
+        }, () => this.beginAnalysis())
+        // this.beginAnalysis();
     }
 
     beginAnalysis() {
-
         const xhr = new XMLHttpRequest();
 
         const url = "http://127.0.0.1:8080/" + localStorage.getItem("sid");
@@ -39,6 +39,7 @@ class TextPage extends Component {
         xhr.send(this.state.entry);
 
         xhr.onreadystatechange = (e) => {
+            
             if (xhr.readyState === 4) {
                 this.getAnalysisResults();
             }
@@ -58,9 +59,11 @@ class TextPage extends Component {
         let that = this;
 
         xhr.onreadystatechange = (e) => {
+            console.log("ready state", xhr.readyState)
             if (xhr.readyState === 4) {
+                console.log("RES", xhr.responseText)
                 that.stateSetter(JSON.parse(xhr.responseText));
-                that.setSearchTerms();
+                // that.setSearchTerms();
             }
         }
     }
@@ -68,28 +71,31 @@ class TextPage extends Component {
     stateSetter(str) {
         this.setState({
             analysisResults: str
-        })
+        }, () => this.setSearchTerms())
     };
 
     setSearchTerms() {
         let terms = [];
-        let analysisLength = this.state.analysisResults.length - 1;
-        this.state.analysisResults[analysisLength]['comments'].forEach(elem => {
+        console.log("STATE", this.state)
+        let analysisLength = this.state.analysisResults.length > 0 ? this.state.analysisResults.length - 1 : 0;
+        if(this.state.analysisResults[analysisLength]){
+            this.state.analysisResults[analysisLength]['comments'].forEach(elem => {
                 terms.push(this.state.previousEntry.slice(elem.start, elem.end))
             }
         )
         this.setState({
             searchTerms: terms
         })
+        }
+       
     }
 
     render() {
-        let output = <div/>;
+        // let output = <div/>;
         let counts = <div/>;
         let moodData = []
-        let moods = ["frustrated", "sad", "satisfied", "excited", "polite", "impolite", "sympathetic"]
-        let analysisLength = this.state.analysisResults.length - 1;
-        if (this.state.analysisResults[analysisLength]['tone']) {
+        let analysisLength = this.state.analysisResults.length > 0 ? this.state.analysisResults.length - 1 : 0;
+        if (this.state.analysisResults[analysisLength] && this.state.analysisResults[analysisLength]['tone']) {
             this.state.analysisResults[analysisLength]['tone'].forEach(result => {
                 if (result.score >= 0.5) {
                     moodData.push(<div className="bold" key={moodData.length}>{result.tone_name}</div>)
@@ -97,7 +103,7 @@ class TextPage extends Component {
             })
         }
 
-        if (this.state.analysisResults[analysisLength].counts) {
+        if (this.state.analysisResults[analysisLength] && this.state.analysisResults[analysisLength].counts) {
             let lines = [];
 
             Object.entries(this.state.analysisResults[analysisLength].counts).forEach((k, v) => {
@@ -109,19 +115,22 @@ class TextPage extends Component {
                 <div>
                     {lines}
                 </div>)
+        } else {
+            counts = <div/>
         }
-        if (this.state.analysisResults) {
-            let comments = []
-            if (this.state.analysisResults[analysisLength]['comments']) {
-                this.state.analysisResults[analysisLength]['comments'].forEach(elem => {
-                    comments.push(<p key={comments.length}>{elem.suggestion}</p>)
-                })
-            }
-            output = (
-                <div>
-                    Comments: {comments}
-                </div>)
-        }
+        let toneAnalysis = moodData.length > 0 ? <div><span className="bold underline">Analysis:</span><div>{moodData}</div></div> :  <div/>
+        // if (this.state.analysisResults) {
+        //     let comments = []
+        //     if (this.state.analysisResults[analysisLength]['comments']) {
+        //         this.state.analysisResults[analysisLength]['comments'].forEach(elem => {
+        //             comments.push(<p key={comments.length}>{elem.suggestion}</p>)
+        //         })
+        //     }
+        //     output = (
+        //         <div>
+        //             Comments: {comments}
+        //         </div>)
+        // }
         return (
             <div className="container">
                 <div className="row">
@@ -148,10 +157,7 @@ class TextPage extends Component {
                 <div>
                     {counts}
                 </div>
-                <div className="bold underline">
-                    Analysis:
-                </div>
-                {moodData}
+                {toneAnalysis}
             </div>
         )
     }
