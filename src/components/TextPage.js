ad319bb3a17f4cabeceb762c35e6cda30a9d182b
Lisa <lisaobrien2009@hotmail.com>
2020-03-07 20:24:49 -0800

    mock object

--- src/components/TextPage.js
+++ src/components/TextPage.js
@@ -4,7 +4,55 @@ import './Styles.css'
 class TextPage extends Component {
     state ={
         entry: "",
-        output:"this will be the output"
+        output:{
+            'comments': [
+                {
+                    'end': 54, 
+                    'start': 47, 
+                    'suggestion': 'Consider removing this.'},
+                {
+                    'end': 66,
+                    'start': 55,
+                    'suggestion': "Consider removing the word 'just'."
+                },
+                {
+                    'end': 33,
+                    'start': 22,
+                    'suggestion': 'Consider removing this.'
+                }
+            ],
+            'counts': {
+                'just': {
+                    'count': 3,
+                    'response': 'This word comes across as passive or apologetic. Consider removing it.'
+                        },
+                'please': {'count': 1,
+                    'response': 'Using this word too often makes requests ' +
+                        'come across as optional. Be polite, but ' +
+                        'ask yourself whether you are requesting a ' +
+                        'favour or a work task.'
+                    },
+                'sorry': {
+                    'count': 2,
+                    'response': 'Check whether these are really things that ' +
+                                'you need to apologize for.'
+                },
+                'think': {'count': 2,
+                            'response': 'Are you confident in your ideas? If so ' +
+                                        'removing or replacing with alternatives ' +
+                                        "such as 'know', or 'am confident'."}},
+            'tone': [
+                {
+                    'score': 0.639146, 
+                    'tone_id': 'polite', 
+                    'tone_name': 'Polite'},
+                {
+                    'score': 0.80092,
+                    'tone_id': 'sympathetic',
+                    'tone_name': 'Sympathetic'
+                }
+            ]
+        }
     }
 
     onChange = (e) => {
@@ -20,6 +68,20 @@ class TextPage extends Component {
     }
 
     render() {
+        let output = <div />
+        if(this.state.output) {
+            let comments = []
+            if(this.state.output['comments']) {
+                this.state.output['comments'].forEach(elem =>{
+                    comments.push(<p>{elem.suggestion}</p>)
+                })
+                console.log(comments)
+            }
+            output = (
+            <div>
+                Comments: {comments}
+            </div>)
+        }
         return(
             <div className="container row">
                 <div className="card col s6 darkYellow lighten-4">
@@ -28,7 +90,7 @@ class TextPage extends Component {
                 </div>
                 <div className="col s6">
                 <p>
-                    {this.state.output}
+                    {output}
                 </p>
                 </div>
             </div>
