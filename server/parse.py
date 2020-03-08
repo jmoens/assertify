a4caa7736b424a8e2ef9b47d2b9100cd27c5e29b
Harlan Colclough <harlan.colclough@gmail.com>
2020-03-07 16:50:28 -0800

    basic parse that gets counts of key words works

--- server/parse.py
+++ server/parse.py
@@ -1,7 +1,28 @@
+from pprint import pprint
+import json
+
 class Parser():
+    def __init__(self):
+        with open('json/phraseMapping.json') as f:
+            self.phrases = json.load(f)
+        with open('json/simpleWordMapping.json') as f:
+            self.words = json.load(f)
+
     def parse(self, text):
+        counts = {}
         comments = []
+        text = text.lower()
         for i in range(len(text)):
             sub = text[i:]
+            for word in self.words:
+                if sub.startswith(word["word"]):
+                    if word["word"] not in counts:
+                        counts[word["word"]] = { "count": 1, "response": word["response"] }
+                    else:
+                        counts[word["word"]]["count"] += 1
 
-        return comments
+        return { "counts": counts, "comments": comments }
+
+if __name__ == '__main__':
+    parser = Parser()
+    pprint(parser.parse("sorry think just just please sorry"))
