f6372a702ec04ae944bb379b6f6e4f325c42b9b6
Lisa <lisaobrien2009@hotmail.com>
2020-03-07 16:57:59 -0800

    textbox onclick

--- src/components/TextPage.js
+++ src/components/TextPage.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import FormControl from '@material-ui/core/FormControl';
 import Input from '@material-ui/core/Input';
 import InputLabel from '@material-ui/core/InputLabel';
+import './Styles.css'
+
 class TextPage extends Component {
     state ={
         entry: ""
@@ -10,24 +12,32 @@ class TextPage extends Component {
     onChange = (e) => {
         this.setState({
             entry: e.target.value
-        }, () => console.log(this.state))
+        })
     }
 
     onSubmit = (e) => {
         e.preventDefault();
-        console.log("Submits")
+        // this.state.entry has the text input
+        console.log(this.state.entry)
     }
 
     render() {
         return(
-            <div className="container">
-                TextPage
+            <div className="container row">
+                <div className="card col s6 deep-purple lighten-4">
                 <form noValidate autoComplete="off" onSubmit={this.onSubmit}>
                     <FormControl>
-                        <InputLabel htmlFor="component-simple">Name</InputLabel>
+                        <InputLabel htmlFor="component-simple">Input</InputLabel>
                         <Input id="component-simple" value={this.state.entry} onChange={this.onChange} onSubmit={this.onSubmit}/>
                     </FormControl>                
                 </form>
+                <a className="waves-effect btn deep-purple lighten-2 waves-light" onClick={this.onSubmit}>Analyze</a>
+                </div>
+                <div className="col s6">
+                <p>
+                    OUTPUT
+                </p>
+                </div>
             </div>
         )
     }
